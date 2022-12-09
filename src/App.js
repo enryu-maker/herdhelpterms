48f6990ff088d872d03008d1ebaefe4d542182d4
enryu-maker <akifkhan60067@gmail.com>
2022-12-09 23:37:24 +0530

    chnages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
       <Routes>
-        <Route exact path="/" element={<Main/>} />
+        <Route path="/" element={<Main/>} />
       </Routes>
   );
 }
